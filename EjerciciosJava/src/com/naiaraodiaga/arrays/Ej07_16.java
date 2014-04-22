c6c44ce46c5fa945895613e8a1885d66fc625b05
naiaraodiaga <naiaraodiaga@gmail.com>
2014-04-22 12:45:08 +0200

    Mayor y menor de un array

---
+++ EjerciciosJava/src/com/naiaraodiaga/arrays/Ej07_16.java
@@ -0,0 +1,26 @@
+package com.naiaraodiaga.arrays;
+
+import java.util.Scanner;
+
+public class Ej07_16 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int size = 10;
+		int []array = new int[size]; 
+		int menor = 100, mayor = 0;
+		
+		
+		for(int i = 0 ; i < size; i++){
+			array[i] = (int) (100*Math.random()+1);
+			if(menor > array[i]) menor = array[i];
+			if(mayor < array[i]) mayor = array[i];
+			System.out.print(array[i] + "\t");
+		}
+		System.out.println("\nEl mayor es: "+mayor);
+		System.out.println("El menor es: "+menor);
+	}
+
+}
