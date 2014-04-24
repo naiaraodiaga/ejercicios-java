b25e4f234af4247cdf4070603b683582af496a02
naiaraodiaga <naiaraodiaga@gmail.com>
2014-04-24 09:00:48 +0200

    Validador NIF

--- EjerciciosJava/src/com/naiaraodiaga/clasesEstandar/ejercicio24/PruebaValidarNIF.java
+++ EjerciciosJava/src/com/naiaraodiaga/clasesEstandar/ejercicio24/PruebaValidarNIF.java
@@ -9,15 +9,14 @@ public class PruebaValidarNIF {
 	 */
 	public static void main(String[] args) {
 		String nif = "";
+
+		System.out.println("Introduzca un NIF v‡lido: ");
+		Scanner entrada = new Scanner(System.in);
+		nif = entrada.next();
 		
-		while(nif.length() != 9){
-			System.out.println("Introduzca un NIF v‡lido: ");
-			Scanner entrada = new Scanner (System.in);
-			nif = entrada.next();
-		}
 		ValidarNIF validador = new ValidarNIF(nif);
 		validador.comprobarNif();
-		
+
 	}
 
 }
